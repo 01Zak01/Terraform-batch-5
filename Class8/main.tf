3ba2aff36da6de75445695c58fc6f3dc9e23bdc9
01Zak01 <zaknomad84@gmail.com>
2024-04-19 01:27:29 +0000

    Class8

---
+++ Class8/main.tf
@@ -0,0 +1,19 @@
+provider aws {
+    region = "us-east-2"
+}
+
+module "vpc" {
+    source = "01Zak01/vpcbatch5/aws"
+    version = "0.0.4"
+    region        = "us-east-2"
+    vpc_cidr      = "10.0.0.0/16"
+    subnet1_cidr  = "10.0.1.0/24"
+    subnet2_cidr  = "10.0.2.0/24"
+    subnet3_cidr  = "10.0.3.0/24"
+    ip_on_launch  = true
+    instance_type = "t2.micro"
+    subnet1_name = "hello1"
+    subnet2_name = "hello2"
+    subnet3_name = "hello3"
+    ports = [22, 80, 443]
+}
